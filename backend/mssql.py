6c2c435c838fdf129431e0ea9b5917e3277bcefa
calvinguyen <cnguyenr42@gmail.com>
2022-04-06 18:48:24 -0500

    commit mssql

---
+++ backend/mssql.py
@@ -0,0 +1,71 @@
+import pyodbc
+from pyodbc import Error
+#from sqlalchemy import create_engine
+#from sqlalchemy.engine import URL
+"""server = '172.26.54.133'
+database = 'Synfoco_Logistic_Management_System'
+username = 'synfoco'
+password = 'synfoco16'
+cursor = cnxn.cursor()"""
+#cnxn = pyodbc.connect('Driver={SQL Server}; Server=172.26.54.133;'
+#                      'Database=Synfoco_Logistic_Management_System;'
+#                      'UID=synfoco;PWD=Synfoco16;''Trusted_connection=no;')
+
+#connection_string = "DRIVER={SQL Server};SERVER=172.26.54.133;DATABASE=Synfoco_Logistic_Management_System;UID=synfoco;PWD=synfoco16;Trusted_Connection=no;"
+#connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+
+#engine = create_engine(connection_url)
+#con = engine.connect()
+
+def create_cnxn(server_name, db_name, user_name, user_password):
+    cnxn = None
+    try:
+        server = server_name
+        database = db_name
+        username = user_name
+        password = user_password
+        cnxn = pyodbc.connect(
+            'DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password+';Trusted_Connection=no;'
+        )
+        print("Connection established successfully.")
+    except Error as e:
+        print(f"The error '{e}' has occured")
+    return cnxn
+
+def execute_query(cnxn, query):
+    cursor = cnxn.cursor()
+    try:
+        cursor.execute(query)
+        cnxn.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+#returns query value list into JSON serializable format
+def execute_read_query(cnxn, query):
+    cursor = cnxn.cursor()
+    result = None
+    query_values = []
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            query_values.append([x for x in row])
+        #EXAMPLE: query_values = [[1, 'Will'], [2, 'John']]
+        return query_values
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+# returns query with a list of dictionaries retrieved from database into serializable format
+def execute_read_dic(cnxn, query):
+    cursor = cnxn.cursor()
+    results = []
+    try:
+        cursor = cursor.execute(query)
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))
+        #EXAMPLE: results = [{'id': 1, 'name': 'Will', }, {'id': 2, 'name': 'John'}]
+        return results
+    except Error as e:
+        print(f"The error '{e}' occurred")
